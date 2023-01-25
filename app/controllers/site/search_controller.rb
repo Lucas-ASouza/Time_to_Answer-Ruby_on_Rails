dcc728d3332c6b27cbac682e05679c7ff3d1df9a
Lucas-ASouza <lucasalves-x@hotmail.com>
2023-01-25 10:48:46 -0300

    Feat: Search mechanism for questions and answers implemented

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,7 +1,17 @@
 class Site::SearchController < SiteController
 
     def questions
-        params[:term]
-        @questions = Question.includes(:answers).page(params[:page]).per(5)
+
+
+        #@questions = Question.includes(:answers).where(description: params[:term]) 
+        #This will only return ocorrunces equal to the :term value
+
+        #@questions = Question.includes(:answers).where("description Like ?", "%#{params[:term]}") 
+        #this will only return ocorrunces that start will the :term value
+
+        @questions = Question.includes(:answers)
+                             .where("description LIKE ?", "%#{params[:term]}%")
+        #this will return ocorrunces that has the :term value anywhere 
+
     end
 end 
