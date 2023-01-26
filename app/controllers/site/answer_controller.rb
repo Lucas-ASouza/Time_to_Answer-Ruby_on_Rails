143924233381b688ea07f09aaacf93ba756670a0
Lucas-ASouza <lucasalves-x@hotmail.com>
2023-01-26 09:00:53 -0300

    Feat: Questions can now be answered
    but please don't chose the wrong one otherwise something weird will happen

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -2,7 +2,7 @@ class Site::AnswerController < SiteController
 
     def question
         
-        @answer = Answer.find{params[:answer]}"
+        @answer = Answer.find{params[:answer_id]}
 
     end
 end
