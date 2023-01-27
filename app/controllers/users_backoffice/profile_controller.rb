c19225efd3576ff99a5f1136c29db72925d5c123
Lucas-ASouza <lucasalves-x@hotmail.com>
2023-01-27 12:46:32 -0300

    Feat: User's Edit page

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -1,5 +1,22 @@
 class UsersBackoffice::ProfileController < UsersBackofficeController
-    def edit
+    before_action :set_user
 
+    def edit
+        
     end
+
+    def update
+        if @user.update(params_user)
+            redirect_to , notice: "User's data updated with success"
+    end
+
+
+    private
+    
+        def set_user
+            @user = User.find(current_user.id)
+        end
+        def params_user
+            params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
+        end 
 end
